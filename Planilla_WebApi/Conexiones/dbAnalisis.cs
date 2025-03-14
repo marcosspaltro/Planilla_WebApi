a64d88f92347bc8c828a5402c06a39dffb145816
nicolasel77 <nicolasel77@gmail.com>
2025-03-14 14:57:44 -0300

    con analisis

---
+++ Planilla_WebApi/Conexiones/dbAnalisis.cs
@@ -0,0 +1,47 @@
+﻿using Planilla_WebApi.Modelos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Planilla_WebApi.Conexiones
+{
+    public class dbAnalisis
+    {
+        
+        SqlConnection sql = new SqlConnection("Data Source=192.168.1.11;Initial Catalog=dbDatos;User Id=Nikorasu;Password=Oficina02");
+
+        public IEnumerable<Ventas>? VentaAnuales()
+        {
+            string cadena = $"SELECT YEAR(Fecha) AS Año, " +
+                $"FORMAT(SUM(Kilos), 'N0', 'es-ES') AS TotalKilos " +
+                $"FROM vw_VentaProductos " +
+                $"WHERE YEAR(Fecha) >= 2002 " +
+                $"GROUP BY YEAR(Fecha) " +
+                $"ORDER BY Año";
+            sql.Open();
+            SqlCommand cmd = new SqlCommand(cadena, sql);
+            cmd.CommandType = CommandType.Text;
+            List<Ventas> _Ventas = new();
+            try
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    _Ventas.Add(new()
+                    {
+                        Tipo = Convert.ToInt32(dr["Año"]),
+                        Kilos = Convert.ToSingle(dr["TotalKilos"])
+                    });
+                }
+                dr.Close();
+                sql.Close();
+                return _Ventas;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                _Ventas = null;
+                return _Ventas;
+            }
+        }
+    }
+}
--- Planilla_WebApi/Controllers/AnalisisController.cs
+++ Planilla_WebApi/Controllers/AnalisisController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Planilla_WebApi.Conexiones;
+using Planilla_WebApi.Modelos;
+
 
 namespace Planilla_WebApi.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AnalisisController : Controller
     {
-        public IActionResult Index()
+        dbAnalisis db = new dbAnalisis();
+
+        // hhtpget para obtener las sucursales
+        [HttpGet]
+        public IList<Ventas> GetVentasAnuales()
         {
-            return View();
+            var ventas = db.VentaAnuales()?.ToList();
+            // Retornar la lista de sucursales
+            return ventas ?? new List<Ventas>();
         }
     }
 }
